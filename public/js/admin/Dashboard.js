f63bc0bdbd53937a06d39c603e099d086c73b0fd
Kier Quebral <kierquebs@gmail.com>
2022-09-20 21:47:54 +0800

    added new function

--- public/js/admin/Dashboard.js
+++ public/js/admin/Dashboard.js
@@ -53,6 +53,15 @@ $(document).ready(function(){
     
   })
 
+
+
+  $("#btn-add-stock").click(function(){
+
+    loadClassification()
+    $("#addNewStockModal").modal('show');
+    
+  })
+
   $("#btn-modal-footer-copy-img").click(function(){
     var url = $(this).val();
     console.log(url)
@@ -62,7 +71,119 @@ $(document).ready(function(){
     
   })
 
+
+  $("input[data-type='currency']").on({
+    keyup: function() {
+      formatCurrency($(this));
+    },
+    blur: function() { 
+      formatCurrency($(this), "blur");
+    }
+  });
+  
+  
+  function formatNumber(n) {
+  // format number 1000000 to 1,234,567
+  return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+  }
+  
+  
+  function formatCurrency(input, blur) {
+  // appends $ to value, validates decimal side
+  // and puts cursor back in right position.
+  
+  // get input value
+  var input_val = input.val();
+  
+  // don't validate empty input
+  if (input_val === "") { return; }
+  
+  // original length
+  var original_len = input_val.length;
+  
+  // initial caret position 
+  var caret_pos = input.prop("selectionStart");
+    
+  // check for decimal
+  if (input_val.indexOf(".") >= 0) {
+  
+    // get position of first decimal
+    // this prevents multiple decimals from
+    // being entered
+    var decimal_pos = input_val.indexOf(".");
+  
+    // split number by decimal point
+    var left_side = input_val.substring(0, decimal_pos);
+    var right_side = input_val.substring(decimal_pos);
+  
+    // add commas to left side of number
+    left_side = formatNumber(left_side);
+  
+    // validate right side
+    right_side = formatNumber(right_side);
+    
+    // On blur make sure 2 numbers after decimal
+    if (blur === "blur") {
+      right_side += "00";
+    }
+    
+    // Limit decimal to only 2 digits
+    right_side = right_side.substring(0, 2);
+  
+    // join number by .
+    input_val = "$" + left_side + "." + right_side;
+  
+  } else {
+    // no decimal entered
+    // add commas to number
+    // remove all non-digits
+    input_val = formatNumber(input_val);
+    input_val = "₱" + input_val;
+    
+    // final formatting
+    if (blur === "blur") {
+      input_val += ".00";
+    }
+  }
+  
+  // send updated string to input
+  input.val(input_val);
+  
+  // put caret back in the right position
+  var updated_len = input_val.length;
+  caret_pos = updated_len - original_len + caret_pos;
+  input[0].setSelectionRange(caret_pos, caret_pos);
+  }
+
 });
+
+function checkThis(str){
+  var num = new Number(str);
+  if(/^[0-9]{0,3}(\.[0-9]{0,2})?$/.test(str) && num > 0){
+      alert('input is valid');
+  } else {
+     alert('input is invalid');
+  }
+}
+
+function isNumberKey(txt, evt) {
+  var charCode = (evt.which) ? evt.which : evt.keyCode;
+  if (charCode == 46) {
+    //Check if the text already contains the . character
+    if (txt.value.indexOf('.') === -1) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    if (charCode > 31 &&
+      (charCode < 48 || charCode > 57))
+      return false;
+  }
+  return true;
+}
+
+
 
 
 var generateQR = function (stockID,dateAdded){
@@ -117,6 +238,23 @@ async function copyImage(imageURL){
   
 }
 
+
+
+function loadClassification(){
+  $.ajax({
+      url: API+'/classification/ListAll',
+      type: 'get',
+      async:false,
+      success : function(req) {
+          var ret = JSON.parse(JSON.stringify(req))
+          console.log(ret)
+          
+      },
+      error : function(){
+      }
+      
+  });
+}
 
 
 
