8c51390bcecc00928c445ba1c113ece3a7e7fe83
Kier Quebral <kierquebs@gmail.com>
2022-12-22 14:53:34 +0800

    fix routes for chnage the page name of dashboard to information of pigs

--- app/setup/Routes.go
+++ app/setup/Routes.go
@@ -11,10 +11,10 @@ import (
 func Routes(app *fiber.App) {
 
 	adminGroup := app.Group("/admin", logger.New())
-	adminGroup.Get("/dashboard/:token", admin.Dashboard)              // /admin/dashboard
-	adminGroup.Get("/purchase-history/:token", admin.PurchaseHistory) // /admin/purchase-history
-	adminGroup.Get("/appointment/:token", admin.Appointment)          // /admin/appointment
-	adminGroup.Get("/login", admin.Login)                             // /admin/dashboard
+	adminGroup.Get("/information-of-pigs/:token", admin.InformationOfPigs) // /admin/dashboard
+	adminGroup.Get("/purchase-history/:token", admin.PurchaseHistory)      // /admin/purchase-history
+	adminGroup.Get("/appointment/:token", admin.Appointment)               // /admin/appointment
+	adminGroup.Get("/login", admin.Login)                                  // /admin/dashboard
 
 	indexGroup := app.Group("/", logger.New())
 	indexGroup.Get("/", client.Index)
